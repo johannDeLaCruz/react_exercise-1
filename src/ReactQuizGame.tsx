a0888f20e7d6ee7c052088fd3f3b4b62c728aa2c
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2023-12-11 00:01:05 -0300

    fix(ReactQuizGame): building the exercise

--- src/ReactQuizGame.tsx
+++ src/ReactQuizGame.tsx
@@ -1,9 +1,52 @@
+import { useState } from "react";
 
+type ItemsToCheck = string[];
+type DataItem = {
+  city: string;
+  country: string;
+};
+type Item = string;
+type GameData = DataItem[];
 
 const ReactQuizGame = () => {
-  return (
-    <div>ReactQuizGame</div>
-  )
-}
+  const gameData: GameData = [
+    { city: "Moscow", country: "Russia" },
+    { city: "Madrid", country: "Spain" },
+    { city: "Berlin", country: "Germany" },
+    { city: "Santiago", country: "Chile" },
+  ];
+  const flattenedData: ItemsToCheck = gameData.flatMap((obj) =>
+    Object.values(obj)
+  );
 
-export default ReactQuizGame
+  const [selectedItems, setSelectedItems] = useState<ItemsToCheck>([]);
+  
+  const checkMatch = (itemsToCheck) => {
+    
+    
+    
+    return
+  }
+
+
+  const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    const item: Item = (e.target as HTMLButtonElement).value;
+    if (selectedItems.length === 0 || selectedItems.length === 2) {
+      setSelectedItems([item]);
+    } else if (selectedItems.length === 1) {
+      setSelectedItems((prevItems) => [...prevItems, item]);
+    }
+  };
+
+  return (
+    <>
+      {flattenedData.map((item, index) => (
+        <button onClick={handleOnClick} key={index} value={item}>
+          {item}
+        </button>
+      ))}
+    </>
+  );
+};
+
+export default ReactQuizGame;
